a91675f8d89941e99a2469154024f4ee42d4aeb9
natalielyn <nlmaxwell@gmail.com>
2020-01-30 21:22:57 -0800

    created cfps server

--- demoCode/events.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/**
- * EventEmitter creates a new instance of an event.
- */
-const EventEmmitter = require('events');
-
-const events = new EventEmmitter();
-
-module.exports = events;
